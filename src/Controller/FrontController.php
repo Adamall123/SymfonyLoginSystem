a7405c5cea649c8c06723667709827bef517e3fb
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-09-25 01:10:23 +0200

    add login and logout user

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
 {
@@ -27,8 +28,18 @@ class FrontController extends AbstractController
       /**
      * @Route("/login", name="login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $helper): Response
     {
-        return $this->render('front/login.html.twig');
+        return $this->render('front/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+    /**
+     * @Route("/logout", name="logout")
+     */
+
+    public function logout(): void 
+    {
+        throw new \Exception('This should never be reached!');
     }
 }
