5765fef4c5ab621e120bcf552d87f24f3d2e9d23
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-09-25 16:18:37 +0200

    add delete account option

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User;
+
 /**
      * @Route("/admin")
      */
@@ -18,6 +20,24 @@ class AdminController extends AbstractController
     public function index(): Response
     {
         return $this->render('admin/my_profile.html.twig');
+    }
+
+     /**
+     * @Route("/delete-account", name="delete_account")
+     */
+    public function deleteAccount(): Response
+    {
+       
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $user = $entityManager->getRepository(User::class)->find($this->getUser()->getId());
+        
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        session_destroy();
+
+        return $this->redirectToRoute('front');
     }
 
       /**
